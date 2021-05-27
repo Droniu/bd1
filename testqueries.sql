b2becc1dff4e172c1b08056e3370c6b3771d464c
Droniu <dron.official@yahoo.com>
2021-05-27 08:40:05 +0200

    first 5 queries, 1st use case to be checked

--- testqueries.sql
+++ testqueries.sql
@@ -23,13 +23,78 @@
 -- 14 Aktualizuje dane o zrealizowanych usługach 
 
 
--- FROM THE END
 -- 14 Update data about services included in orders
--- We add 
-INSERT INTO Services_Orders
-VALUES (1, 1) 
---input1 = serviceID
---input2 = orderID
+    INSERT INTO Services_Orders
+    VALUES (1, 2);
+    --input1 = serviceID
+    --input2 = orderID
+
+-- 13 Upate client data
+    UPDATE Client
+    SET firstName = "newName"
+    WHERE ClientID = 2
+    --input1 = newName
+    --input2 = ClientID
+
+
+-- 12 Update car service date
+
+-- 11 Browse car parts
+
+-- 10 Browse tasks 
+    SELECT
+        Tasks.taskID
+        Tasks.orderID
+        Tasks.taskDescription
+    FROM Employees_Tasks
+    INNER JOIN Tasks
+    ON Employees_Tasks.taskID = Tasks.taskID
+    WHERE Employees_Tasks.EmployeeID = 1
+    --input1 = EmploeeID
+
+
+-- 9 Set order status
+
+-- 8 Edit list of parts to buy
+    -- OPTIONAL (only if part doesn't yet exist in database)
+    INSERT INTO Parts (price, qty, name, manufacturer)
+    VALUES (1, 2, "3", "4");
+    -- four inputs
+
+    INSERT INTO Parts_Orders
+    VALUES (1, 2);
+    -- two inputs (partID, orderID)
+
+
+-- 7 Check order status
+
+-- 6 Check car service date
+
+-- 5 Browse services
+
+-- 4 Assign tasks to employees
+
+-- 3 Edit employees data
+
+-- 2 Edit services
+
+-- 1 Check parts that need to be bought
+-- nie jestem pewny czy to zadziała, trzeba przetestować
+-- jak będziemy mieć inserty
+    SELECT 
+        Parts.name AS "Part"
+        Parts.manufacturer AS "Manufacturer"
+    FROM Parts_Orders
+    LEFT JOIN Parts
+    ON Parts.partID = Parts_Orders.PartspartID
+    WHERE Parts_Orders.orderID IN (
+        SELECT orderID
+        FROM Orders
+        WHERE status <> 'Completed'
+    );
+
+
+
 
 
 
