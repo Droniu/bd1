53e9db2657ebbf6bff8a918c55f0fcf75e4956e5
Droniu <dron.official@yahoo.com>
2021-05-27 05:51:05 +0200

    employees, client, car inserts

---
+++ inserts.sql
@@ -0,0 +1,64 @@
+-- EMPLOYEES TABLE
+
+insert into employees (firstName, lastName, salary) values ('Anatola', 'Morecombe', 7000);
+insert into employees (firstName, lastName, salary) values ('Colette', 'Mackill', 4700);
+insert into employees (firstName, lastName, salary) values ('Pincus', 'Patchett', 6300);
+insert into employees (firstName, lastName, salary) values ('Evanne', 'Gussin', 5800);
+insert into employees (firstName, lastName, salary) values ('Richy', 'Philipsohn', 7000);
+insert into employees (firstName, lastName, salary) values ('Waylen', 'Goodlatt', 5600);
+insert into employees (firstName, lastName, salary) values ('Tuesday', 'Macauley', 4700);
+insert into employees (firstName, lastName, salary) values ('Marietta', 'Ashmore', 4300);
+insert into employees (firstName, lastName, salary) values ('Clarissa', 'Bartzen', 2600);
+insert into employees (firstName, lastName, salary) values ('Mamie', 'Sprowles', 5600);
+
+-- CLIENTS TABLE
+
+insert into client (ClientID, firstName, lastName) values (1, 'Stoddard', 'Surfleet');
+insert into client (ClientID, firstName, lastName) values (2, 'Aluin', 'Andras');
+insert into client (ClientID, firstName, lastName) values (3, 'Zenia', 'Reinmar');
+insert into client (ClientID, firstName, lastName) values (4, 'Allyce', 'Clynman');
+insert into client (ClientID, firstName, lastName) values (5, 'Terese', 'Cammacke');
+insert into client (ClientID, firstName, lastName) values (6, 'Ophelie', 'Anker');
+insert into client (ClientID, firstName, lastName) values (7, 'Marijo', 'Texton');
+insert into client (ClientID, firstName, lastName) values (8, 'Enoch', 'Huckfield');
+insert into client (ClientID, firstName, lastName) values (9, 'Martha', 'Brazer');
+insert into client (ClientID, firstName, lastName) values (10, 'Jorry', 'Stapford');
+insert into client (ClientID, firstName, lastName) values (11, 'Danny', 'Shillito');
+insert into client (ClientID, firstName, lastName) values (12, 'Goldi', 'Simonini');
+insert into client (ClientID, firstName, lastName) values (13, 'Marco', 'Zoellner');
+insert into client (ClientID, firstName, lastName) values (14, 'Cece', 'Conechie');
+insert into client (ClientID, firstName, lastName) values (15, 'Amby', 'Cobello');
+insert into client (ClientID, firstName, lastName) values (16, 'Aurthur', 'Slark');
+insert into client (ClientID, firstName, lastName) values (17, 'Brewster', 'Thorsby');
+insert into client (ClientID, firstName, lastName) values (18, 'Romonda', 'Edser');
+insert into client (ClientID, firstName, lastName) values (19, 'Else', 'Boniface');
+insert into client (ClientID, firstName, lastName) values (20, 'Colas', 'McAvaddy');
+
+-- CARS TABLE
+
+insert into car (ClientID, model, year, serviceDate) values (1, 'E-Class', 1999, '2021-05-22');
+insert into car (ClientID, model, year, serviceDate) values (2, 'Reliant', 1981, '2021-05-20');
+insert into car (ClientID, model, year, serviceDate) values (3, 'Maxima', 2007, '2021-05-20');
+insert into car (ClientID, model, year, serviceDate) values (4, 'Esperante', 2007, '2021-05-25');
+insert into car (ClientID, model, year, serviceDate) values (5, 'SLK-Class', 2004, '2021-05-24');
+insert into car (ClientID, model, year, serviceDate) values (6, 'Ram 1500', 2004, '2021-05-20');
+insert into car (ClientID, model, year, serviceDate) values (7, 'Sonata', 1997, '2021-05-25');
+insert into car (ClientID, model, year, serviceDate) values (8, '944', 1988, '2021-05-21');
+insert into car (ClientID, model, year, serviceDate) values (9, 'MPV', 1992, '2021-05-20');
+insert into car (ClientID, model, year, serviceDate) values (10, 'RL', 2003, '2021-05-25');
+insert into car (ClientID, model, year, serviceDate) values (11, 'Thunderbird', 2006, '2021-05-22');
+insert into car (ClientID, model, year, serviceDate) values (12, 'Grand Caravan', 2008, '2021-05-24');
+insert into car (ClientID, model, year, serviceDate) values (13, 'Explorer Sport Trac', 2010, '2021-05-20');
+insert into car (ClientID, model, year, serviceDate) values (14, 'Fit', 2010, '2021-05-21');
+insert into car (ClientID, model, year, serviceDate) values (15, 'LTD Crown Victoria', 1987, '2021-05-20');
+insert into car (ClientID, model, year, serviceDate) values (16, 'G-Class', 2008, '2021-05-22');
+insert into car (ClientID, model, year, serviceDate) values (17, 'Villager', 2000, '2021-05-22');
+insert into car (ClientID, model, year, serviceDate) values (18, 'Topaz', 1992, '2021-05-20');
+insert into car (ClientID, model, year, serviceDate) values (19, 'Neon', 1997, '2021-05-23');
+insert into car (ClientID, model, year, serviceDate) values (20, 'Voyager', 1999, '2021-05-24');
+insert into car (ClientID, model, year, serviceDate) values (7, 'Sable', 1990, '2021-05-22');
+insert into car (ClientID, model, year, serviceDate) values (3, 'Sequoia', 2002, '2021-05-24');
+insert into car (ClientID, model, year, serviceDate) values (2, 'C-Class', 2009, '2021-05-22');
+insert into car (ClientID, model, year, serviceDate) values (2, 'LS Hybrid', 2011, '2021-05-24');
+insert into car (ClientID, model, year, serviceDate) values (1, 'Murciélago', 2004, '2021-05-25');
+
